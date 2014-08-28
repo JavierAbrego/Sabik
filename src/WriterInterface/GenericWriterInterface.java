2326df3e0b6298ec6871d79e61676a1cd23fb02d
JavierAbrego <jabrego@playitapp.es>
2014-08-28 19:31:50 +0200

    New eception implemented in GenericWriterInterface

--- src/WriterInterface/GenericWriterInterface.java
+++ src/WriterInterface/GenericWriterInterface.java
@@ -1,21 +1,21 @@
 package WriterInterface;
 
+import Exception.DBManagerException;
 import java.util.List;
-
+import Exception.VariableManagerException;
 
 public interface GenericWriterInterface {
     
-    public void header(String Path);
-    public void getInputVars(String Path, List<String> input);
-    public void postInputVars(String Path, List<String> input);
-    public void useDatabase(String alias, String Path);
-    public void addDatabase(String alias, String host, String port, String user, String passsword, String databaseName);
+    public void header(String path);
+    public void getInputVars(String path, List<String> input) throws VariableManagerException;
+    public void postInputVars(String path, List<String> input) throws VariableManagerException;
+    public void useDatabase(String alias, String path);
+    public void addDatabase(String alias, String host, String port, String user, String passsword, String databaseName) throws DBManagerException;
     public void executeSqlQuery(String Path, String Query);
-    public void writeSqlQueryAndGetResultInVariable();
-    public void writeSqlQueryAndReturnBoolean();
-    public void writeSqlQueryAndReturnRows();
+    public void executeSqlQueryAndGetResultInVariable(String path, String query, String variableName) throws VariableManagerException;   
+    public void countResultRowsNumberAndGetResultInVariable(String path, String rowsVariableName, String resultVariableName) throws VariableManagerException;
     public void writeArithmeticAndReturn(String Arithmetic);
     public void writeArithmeticAndGetResultInVariable(String Arithmetic);
-    public void EOF(String Path);
+    public void EOF(String path);
     
 }
