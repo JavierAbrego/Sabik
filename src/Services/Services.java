d66edb7fe247f3697bf799b5e9a26f571e789cd1
JavierAbrego <jabrego@playitapp.es>
2014-09-07 17:06:15 +0200

    Implemented facade for creating tasks

--- src/Services/Services.java
+++ src/Services/Services.java
@@ -1,91 +1,54 @@
 package Services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
 import Exception.DBManagerException;
 import Exception.VariableManagerException;
+import Facade.TaskFacade;
 import Model.Job;
-import Model.Tasker;
-import Model.Variable;
-import Model.VariableList;
 import Model.WebServiceBatch;
 
 
 public class Services {
 
     public static void main(String[] args) {
-        //Creation of batch of processes
+        //CREATE BATCH OF PROCESSES
         WebServiceBatch batch =  new WebServiceBatch();
         
-        //Creation of a Job
+        //CREATE JOB
         Job job =  new Job();
         job.setJobName("test Job");
         //Path for Job
-        Variable path =  new Variable.VariableBuilder()
-									.name("path")
-									.value("test.php")
-									.build();
-
-        //HEADER
-        //definition of parameters
-        VariableList parameters = new VariableList();
-        parameters.addVariable(path);
-        //creation of task header
-        Tasker tasker =  new Tasker.TaskerBuilder()
-        							.task(Tasker.Task.header)
-        							.parameters(parameters)
-        							.build();
-        job.addTasker(tasker);
-        
-        //BEGIN TRANSACTION
-        //definition of parameters
-        VariableList parameters2 = new VariableList();
-        parameters2.addVariable(path);
-        //creation of task header
-         tasker =  new Tasker.TaskerBuilder()
-        							.task(Tasker.Task.beginTransaction)
-        							.parameters(parameters2)
-        							.build();
-        job.addTasker(tasker);
+        String path =  "test.php";
         
         
-        //END TRANSACTION
-        //definition of parameters
-        VariableList parameters3 = new VariableList();
-        parameters3.addVariable(path);
-        //creation of task header
-         tasker =  new Tasker.TaskerBuilder()
-        							.task(Tasker.Task.endTransaction)
-        							.parameters(parameters3)
-        							.build();
-        job.addTasker(tasker);
-        //EOF
-        //definition of parameters
-         VariableList parameters4 = new VariableList();
-        parameters4.addVariable(path);
-        Variable test2 =  new Variable.VariableBuilder()
-							.name("test2")
-							.value("test.php")
-							.build();
-        parameters4.addVariable(test2);
-        //creation of task header
-        tasker =  new Tasker.TaskerBuilder()
-        							.task(Tasker.Task.EOF)
-        							.parameters(parameters4)
-        							.build();
-        job.addTasker(tasker);
+        //add tasks for jobs
+        TaskFacade.header(job, path);
+        TaskFacade.beginTransaction(job, path);
+        @SuppressWarnings("serial")
+		List<String> inputVars = new ArrayList<String>() {{ add("input"); add("action"); }};
+        TaskFacade.getInputVars(job, path, inputVars, TaskFacade.RequestMethod.GET);
+        TaskFacade.getInputVars(job, path, inputVars, TaskFacade.RequestMethod.POST);
+        TaskFacade.addDatabase(job, path, "alias", "host", "port", "user", "password", "databaseName");
+        TaskFacade.useDatabase(job, path, "alias");
+        TaskFacade.endTransaction(job, path);
+        TaskFacade.EOF(job, path);
         
-        
-        //ADD JOBS TO BATCH  and execute
+        //ADD JOBS TO BATCH  
         batch.addJob(job);
+        //EXECUTE BATCH
         try {
 			batch.start();
 		} catch (VariableManagerException | DBManagerException e1) {
 			e1.printStackTrace();
 		}
         
+        //PRINT BATCH OBJECT TO XML
         try {
 			JAXBContext jc = JAXBContext.newInstance(WebServiceBatch.class);
 			 Marshaller marshaller = jc.createMarshaller();
@@ -93,7 +56,6 @@ public class Services {
 			 marshaller.marshal(batch, System.out);
 			
 		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
